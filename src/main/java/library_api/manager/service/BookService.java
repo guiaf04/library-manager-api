895149c2b7a94b59fe8f26021c7d57de1f1dbbd5
guiaf04 <guilhermearaujo@alu.ufc.br>
2024-06-16 11:41:56 -0300

    feat: add book service

---
+++ src/main/java/library_api/manager/service/BookService.java
@@ -0,0 +1,7 @@
+package library_api.manager.service;
+
+import library_api.manager.domain.model.Book;
+
+public interface BookService extends CrudService<Book,Long>{
+
+}
---
+++ src/main/java/library_api/manager/service/Impl/BookServiceImpl.java
@@ -0,0 +1,38 @@
+package library_api.manager.service.Impl;
+
+import library_api.manager.domain.model.Book;
+import library_api.manager.domain.repository.BookRepository;
+import library_api.manager.service.BookService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book findById(Long id) {
+        return bookRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public Book create(Book book) {
+        if(bookRepository.existsBookByISBN(book.getISBN())){
+            throw new IllegalArgumentException("This book already exists");
+        }
+        return bookRepository.save(book);
+    }
+
+}
